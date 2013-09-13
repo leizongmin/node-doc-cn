9de04b3d315cc23a42eab779f1b4d5ded862ff17
Zongmin Lei <leizongmin@gmail.com>
2013-09-13 13:37:10 +0800

    all.markdown

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var express = require('express');
 var http = require('http')
 var path = require('path');
 var fs = require('fs');
+var async = require('async');
 var api2HTML = require('./tools/html');
 var api2JSON = require('./tools/json');
 
@@ -25,17 +26,68 @@ app.configure(function(){
   app.use(express.errorHandler());
 });
 
+
+var TEMPLATE_FILE = path.resolve(__dirname, 'tools/template.html');
+var API_PATH = path.resolve(__dirname, 'api-en');
+
+// 取API文件名
+function resolveAPIPath (name) {
+  return path.resolve(API_PATH, name + '.markdown');
+}
+
+// 读取文件内容
+function readAPIFile (name, callback) {
+  if (name === 'all') {
+    var filename = resolveAPIPath(name);
+    fs.readFile(filename, 'utf8', function (err, content) {
+      if (err) return callback(err);
+      processIncludes(content, function (err, content) {
+        callback(err, content, filename);
+      });
+    });
+  } else {
+    var filename = resolveAPIPath(name);
+    fs.readFile(filename, 'utf8', function (err, content) {
+      callback(err, content, filename);
+    });
+  }
+}
+
+// 处理包含文件
+function processIncludes (content, callback) {
+  var includeExpr = /^@include\s+([A-Za-z0-9-_]+)(?:\.)?([a-zA-Z]*)$/gmi;
+  var includes = content.match(includeExpr);
+  if (includes === null) return cb(null, content);
+
+  includes = includes.map(function(include) {
+    var fname = include.replace(/^@include\s+/, '');
+    if (fname.match(/\.markdown$/)) fname = fname.slice(0, -9);
+    return fname;
+  });
+
+  var allContent = [];
+  async.eachSeries(includes, function (name, next) {
+    readAPIFile(name, function (err, content, filename) {
+      if (!err) {
+        allContent.push(content);
+      }
+      next();
+    });
+  }, function (err) {
+    if (err) return callback(err);
+    callback(null, allContent.join('\n'));
+  });
+}
+
 app.get('/api/:name.:type', function (req, res, next) {
   var name = req.params.name;
   var type = req.params.type;
-
   if (name === 'index') name = '_toc';
 
-  var filename = path.resolve(__dirname, 'api-en', name + '.markdown');
-  fs.readFile(filename, 'utf8', function (err, content) {
+  readAPIFile(name, function (err, content, filename) {
     if (err) return next(err);
     if (type === 'html') {
-      api2HTML(content, filename, path.resolve(__dirname, 'tools/template.html'), function (err, html) {
+      api2HTML(content, filename, TEMPLATE_FILE, function (err, html) {
         if (err) return next(err);
         res.writeHead(200, {'content-type': 'text/html'});
         res.end(html);
