b1e480a3a62ff3896a85e0139ec06c49b8ce5062
Zongmin Lei <leizongmin@gmail.com>
2013-09-14 22:09:30 +0800

    赞 和 编辑 别人的翻译

--- routes/edit_api.js
+++ routes/edit_api.js
@@ -108,4 +108,30 @@ module.exports = function (app) {
     });
   });
 
+  app.post('/translate/vote', check_signin, function (req, res, next) {
+    var id = req.body.id;
+    if (!(id > 0)) return res.json({error: 'id参数有误'});
+
+    var user_id = req.signinUser.id;
+    var where = '`translate_id`=' + db.escape(id) + ' AND `user_id`=' + db.escape(user_id);
+    db.selectOne('translate_vote_history', '*', where, function (err, vote) {
+      if (err) return res.json({error: err.toString()});
+      if (vote) return res.json({success: 0});
+
+      db.insert('translate_vote_history', {
+        translate_id: id,
+        user_id:      user_id
+      }, function (err, ret) {
+        if (err) return res.json({error: err.toString()});
+
+        var sql = 'UPDATE `translate_api` SET `vote`=`vote`+1 WHERE `id`=' + db.escape(id);
+        db.query(sql, function (err, ret) {
+          if (err) return res.json({error: err.toString()});
+
+          res.json({success: ret.affectedRows});
+        });
+      });
+    });
+  });
+
 };
